f1698f298b69ad42e6f3d82e81c6b053102b2b3b
xnubar <nxalidova@std.qu.edu.az>
2019-04-21 16:30:21 +0400

    [+] script.js firebase config is added

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var config = {
+    apiKey: "AIzaSyBnzR4VgkD81ANrPCVTD6xu8vnorJWz_II",
+    authDomain: "test-a05b4.firebaseapp.com",
+    databaseURL: "https://test-a05b4.firebaseio.com",
+    projectId: "test-a05b4",
+    storageBucket: "test-a05b4.appspot.com",
+    messagingSenderId: "131623986672"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+var player1;
+var player2;
+var playerText = "player";
+
+var winner;
+var loser;
+
+
+
